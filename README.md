76099d09fd67e875a25b192885d921cff69b90ba
leoatchina <leoatchina@gmail.com>
2019-11-27 18:34:24 +0800

    update, add -e

--- README.md
+++ README.md
@@ -1,85 +1,57 @@
 # py2so
-[English](https://github.com/cckuailong/py2so/blob/master/README.md) || 
-[中文](https://github.com/cckuailong/py2so/blob/master/README_zh.md)
+为了加密做出来的项目，原来用Pyinstaller但是发现完全没加密，
+
+后来找到了[https://github.com/cckuailong/py2so](https://github.com/cckuailong/py2so)， fork 后根据自己的需要进行了改动
+
+主要是指定库文件目录，以及指定是哪个大版本的python, 以及强制删除源python文件。
+
+## 例子
+```
+python py2so.py -l ~/anaconda3/include/python3.6m -d ~/source/server -o ~/release/server -m server.py
+```
+会先把`~/source/server`里的全部文件同步到`~/release/server`，然后把除了`server.py`外的所有py文件编译成`.so`文件
 
 ## py2so简介
 1. py2so可以将python文件转化为so文件，达到加密python文件的目的
 2. py2so加密一个py文件，也可以直接加密一整个python项目
-3. .py生成的.so文件可以被主文件通过 "from module import \*" 调用
-4. py2so可以自动识别项目中的py文件, 如果项目中某些文件你不想加密，py2so也可以实现你的目的
-5. 你可以选择是否保留加密前的文件或项目
-6. 考虑到用户误操作，默认会保留加密前的项目，项目名后缀为 "_old"
-7. py2so 支持 python2 and python3, py2 --> py2so.py    py3 --> py2so_py3.py
+3. 生成的.so文件可以被主文件通过 "from module import \*" 调用
+4. 可以自动识别项目中的py文件,可以指定哪些文件或文件夹不被加密
+5. 强制在目标文件夹删除加密过的py文件
+6. 采用了把源文件目录同步到指定输出文件夹的方式，默认是 "./output"
+7. 可以指定用python2或者python3，默认是 python3
+8. 碰到无法编译的情况会退出
 
-## 环境配置
+## 安装cython
 ```
-sudo bash config.sh
+sudo bash install_cython.sh
 ```
-安装一些必要的库
+安装cython
 
 ## 使用说明
-#### Python2
+
 ```
-使用: 
   python py2so.py [选项] ...
 ```
 
-```
 选项:
-  -v,  --version    显示py2so版本
+```
   -h,  --help       显示帮助菜单
-  -p,  --py         Python的子版本号, 默认值为 7
-                    例: -p 7  (比如你使用python2.7)
-  -d,  --directory  Python项目路径 (如果使用-d参数, 将加密整个Python项目)
+  -l,  --lib        指定要include的python库文件,必填。这个是和源文件的重要区别之一
+  -p,  --py         Python的子版本号, 默认值为 3。 次重要区别
+                    例: -p 2  (比如你使用python2)
+  -d,  --directory  Python项目路 径 (如果使用-d参数, 将加密整个Python项目)
   -f,  --file       Python文件 (如果使用-f, 将加密单个Python文件)
-  -c,  --clear      删除原文件（项目）
-                    (警告: -c参数将会删除原文件, 但是为了避免损失，py2so会自动备份原文件)
+                    -d 或 -f 必填
+  -o,  --output     指定输出目录，如果不存在会自动建立。默认是当前目录下的.output文件夹
   -m,  --maintain   标记你不想加密的文件或文件夹路径
-                    注意: 文件夹路径需要使用'[]'包起来, 并且需要和-d参数一起使用 
+                    注意: 文件夹路径需要使用'[]'包起来, 并且需要和-d参数一起使用
                     例: -m __init__.py,setup.py,[poc,resource,venv,interface]
 ```
 
-```
 例:
+```
   python py2so.py -f test_file.py
-  python py2so.py -d ../test_dir -m __init__.py -c
+  python py2so.py -d ../test_dir -m __init__.py
   python py2so.py -d /home/test/test_dir -m [poc/,resource/,venv/,interface/]
-  python py2so.py -d test_dir -m __init__.py,setup.py,[poc/,resource/,venv/,interface/] -c
+  python py2so.py -d test_dir -m __init__.py,setup.py,[poc/,resource/,venv/,interface/]
 ```
-#### Python3
-```
-使用: 
-  python py2so_py3.py [选项] ...
-```
-
-```
-选项:
-  -v,  --version    显示py2so_py3版本
-  -h,  --help       显示帮助菜单
-  -p,  --py         Python的子版本号, 默认值为 7
-                    例: -p 7  (比如你使用python3.7)
-  -d,  --directory  Python项目路径 (如果使用-d参数, 将加密整个Python项目)
-  -f,  --file       Python文件 (如果使用-f, 将加密单个Python文件)
-  -c,  --clear      删除原文件（项目）
-                    (警告: -c参数将会删除原文件, 但是为了避免损失，py2so_py3会自动备份原文件)
-  -m,  --maintain   标记你不想加密的文件或文件夹路径
-                    注意: 文件夹路径需要使用'[]'包起来, 并且需要和-d参数一起使用 
-                    例: -m __init__.py,setup.py,[poc,resource,venv,interface]
-```
-
-```
-例:
-  python py2so_py3.py -f test_file.py
-  python py2so_py3.py -d ../test_dir -m __init__.py -c
-  python py2so_py3.py -d /home/test/test_dir -m [poc/,resource/,venv/,interface/]
-  python py2so_py3.py -d test_dir -m __init__.py,setup.py,[poc/,resource/,venv/,interface/] -c
-```
-
-
-整个Python项目加密前:
-
-![demo1](https://github.com/cckuailong/py2so/blob/master/img/1.png)
-
-py2so加密后效果:
-
-![demo2](https://github.com/cckuailong/py2so/blob/master/img/2.png)
--- py2so.py
+++ py2so.py
@@ -14,11 +14,9 @@ def transfer(dir_pref):
         else:
             print("Compilie error, please check you have installed cython or the lib_dir is right")
         sys.exit(1)
+    os.system('rm -f %s.c %s.o %s.py' % (dir_pref, dir_pref, dir_pref))
     print("Completed %s" % dir_pref)
-    if clear:
-        os.system('rm -f %s.c %s.o %s.py' % (dir_pref, dir_pref, dir_pref))
-    else:
-        os.system('rm -f %s.c %s.o' % (dir_pref, dir_pref))
+
 
 if __name__ == '__main__':
     help_show = '''
@@ -29,32 +27,31 @@ py2so needs the environment of python2
 Usage: python py2so.py [options] ...
 
 Options:
-  -v,  --version    Show the version of the py2so
   -h,  --help       Show the help info
   -p,  --py         Python version, default value is 3
                     Example: -p 2  (means you use python2)
   -l,  --lib        python libray for compile, must be offered
   -d,  --directory  Directory of your project (if use -d, you change the whole directory)
+  -e,  --exclude    Directories or files that you do not want to sync to output file
+                    dirs __pycache__, .vscode, .git, .idea will always not be synced
   -f,  --file       File to be transfered (if use -f, you only change one file)
   -o,  --output     Directory to store the compile results, default "./output"
-  -c,  --clear      Clear the origin .py and .git directory
-                    (Warning: the option will delete the .py, but don't be so serious, py2so will give the project a copy)
   -m,  --maintain   List the file or the directory you don't want to transfer
                     Note: The directories should be surrounded by '[]', and must be the relative path to -d's value
                     Example: -m __init__.py,setup.py,[poc,resource,venv,interface]
 Example:
   python py2so.py -f test_file.py
-  python py2so.py -d test_dir -m __init__.py,setup.py,[poc/,resource/,venv/,interface/] -c
+  python py2so.py -d test_dir -m __init__.py,setup.py,[poc/,resource/,venv/,interface/]
     '''
-    clear      = 0
-    py_ver     = '3'
-    source_dir = ''
-    file_name  = ''
-    m_list     = ''
-    lib_dir    = ''
-    output_dir = './output'
+    py_ver       = '3'
+    source_dir   = ''
+    file_name    = ''
+    m_list       = ''
+    lib_dir      = ''
+    exclude_list = []
+    output_dir   = './output'
     try:
-        options,args = getopt.getopt(sys.argv[1:],"vhcp:l:o:d:f:m:",["version","help","clear","lib=","py=","directory=","file=","output=","maintain="])
+        options,args = getopt.getopt(sys.argv[1:],"hp:l:o:d:f:m:e:",["help","py=","lib=","file=","output=","directory=","maintain=","exclude="])
     except getopt.GetoptError:
         print('Get options Error')
         print(help_show)
@@ -63,10 +60,7 @@ Example:
     for key, value in options:
         if key in ['-h', '--help']:
             print(help_show)
-        elif key in ['-v', '--version']:
-            print('py2so version 0.0.2')
-        elif key in ['-c', '--clear']:
-            clear = 1
+            sys.exit(0)
         elif key in ['-p', '--py']:
             p_subv = value
         elif key in ['-l', '--lib']:
@@ -77,6 +71,8 @@ Example:
             source_dir = value
         elif key in ['-f', '--file']:
             file_name = value
+        elif key in ['-e', '--exclude']:
+            exclude_list = value.split(",")
         elif key in ['-m', '--maintain']:
             m_list = value
             file_flag = 0
@@ -95,14 +91,21 @@ Example:
                 file_flag = 1
             if dir_flag == 1:
                 dir_tmp = dir_list.split(',')
-                dir_list=[]
+                dir_list = []
                 for d in dir_tmp:
                     if d.startswith('./'):
                         dir_list.append(d[2:])
                     else:
                         dir_list.append(d)
+    exclude_list = set(['.git', '.vscode', '.idea', '__pycache__'] + exclude_list)
+    exclude_list = "'"+ "', '".join(exclude_list) + "'"
+
     if lib_dir[-1] == r'/':
         lib_dir = lib_dir[:-1]
+
+    if not os.path.isdir(lib_dir):
+        print('lib_dir must be given, useing -l or --lib')
+        sys.exit(1)
 
     if source_dir[-1] == r'/':
         source_dir = source_dir[-1]
@@ -110,30 +113,28 @@ Example:
     if output_dir[-1] == r'/':
         output_dir = output_dir[-1]
 
+    if os.path.abspath(source_dir) == os.path.abspath(output_dir):
+        print("Source dir equals output dir!")
+        sys.exit(1)
+
     if not py_ver in ['2', '3']:
         print('python version must be 3 or 2')
         sys.exit(1)
-    if not os.path.isdir(lib_dir):
-        print('lib_dir must be given, useing -l or --lib')
-        sys.exit(1)
 
-    if source_dir != '':
+    if source_dir:
         if not os.path.exists(source_dir):
             print('No such Directory, please check or use the Absolute Path')
             sys.exit(1)
         if not os.path.exists(output_dir):
             try:
                 os.system('mkdir -p %s' % output_dir)
-            except Exception as err:
+            except Exception:
                 print("Cannot mkdir -p %s" % output_dir)
                 sys.exit(1)
+        os.system("rsync -azP ",
+                  "--exclude={%s} " % exclude_list,
+                  "--delete %s/ %s/" % (source_dir, output_dir))
 
-        os.system("rsync -azP --delete %s/ %s/" % (source_dir, output_dir))
-        if clear:
-            try:
-                os.system("rm -rf %s/.git*" % output_dir)
-            except Exception as e:
-                pass
         try:
             for root, dirs, files in os.walk(output_dir):
                 for file in files:
