daabedb3a7143fca7693e4e71846ac5fe11042d5
leoatchina <leoatchina@gmail.com>
2020-06-06 10:05:47 +0800

    update

--- py2so.py
+++ py2so.py
@@ -132,9 +132,7 @@ Example:
             except Exception:
                 print("Cannot mkdir -p %s" % output_dir)
                 sys.exit(1)
-        os.system("rsync -azP ",
-                  "--exclude={%s} " % exclude_list,
-                  "--delete %s/ %s/" % (source_dir, output_dir))
+        os.system("rsync -azP --exclude={%s} --delete %s/ %s/" % (exclude_list, source_dir, output_dir))
         try:
             for root, dirs, files in os.walk(output_dir):
                 for file in files:
